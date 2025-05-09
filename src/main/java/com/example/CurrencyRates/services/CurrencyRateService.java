aa5e085832a25da26232420295a74811875fe20f
medovmisha <medovmikhail03@mail.ru>
2025-05-09 12:29:52 +0300

    add services

---
+++ src/main/java/com/example/CurrencyRates/services/CurrencyRateService.java
@@ -0,0 +1,89 @@
+package com.example.CurrencyRates.services;
+
+import com.example.CurrencyRates.dto.CurrencyDTO;
+import com.example.CurrencyRates.dto.CurrencyRateDTO;
+import com.example.CurrencyRates.entities.CurrencyRate;
+import com.example.CurrencyRates.repositories.CurrencyRateRepository;
+import com.example.CurrencyRates.utils.mappers.CurrencyRateMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class CurrencyRateService {
+
+    @Autowired
+    private CurrencyRateMapper currencyRateMapper;
+    @Autowired
+    private CurrencyRateRepository currencyRateRepository;
+    @Autowired
+    private CurrencyService currencyService;
+
+    public List<CurrencyRateDTO> getCurrencyRates() {
+        return currencyRateRepository.findAll()
+                .stream()
+                .map(currencyRateMapper::fromEntityToDTO)
+                .toList();
+    }
+
+    public CurrencyRateDTO getCurrencyRate(Long id) {
+        return currencyRateMapper.fromEntityToDTO(
+                currencyRateRepository.findById(id).orElse(null)
+        );
+    }
+
+    public CurrencyRateDTO addCurrencyRate(Long currencyId, CurrencyRateDTO currencyRateDTO) {
+        if (currencyRateRepository.findByCurrencyId(currencyId).isPresent()) return null;
+        currencyRateDTO.setCurrencyId(currencyId);
+        currencyRateDTO.setDate(new Date());
+        CurrencyRate currencyRate = currencyRateMapper.fromDTOtoEntity(currencyRateDTO);
+        try {
+            currencyRateDTO = currencyRateMapper.fromEntityToDTO(
+                    currencyRateRepository.save(currencyRate)
+            );
+        } catch (RuntimeException e) {
+            return null;
+        }
+        return currencyRateDTO;
+    }
+
+    public CurrencyRateDTO addCurrencyRate(String code, CurrencyRateDTO currencyRateDTO) {
+        if (currencyRateRepository.findByCurrencyCode(code).isPresent()) return null;
+
+        CurrencyDTO currencyDTO = currencyService.getCurrency(code.toUpperCase());
+
+        if (currencyDTO == null) return null;
+
+        currencyRateDTO.setCurrencyId(currencyDTO.getId());
+        currencyRateDTO.setDate(new Date());
+        CurrencyRate currencyRate = currencyRateMapper.fromDTOtoEntity(currencyRateDTO);
+        try {
+            currencyRateDTO = currencyRateMapper.fromEntityToDTO(
+                    currencyRateRepository.save(currencyRate)
+            );
+        } catch (RuntimeException e) {
+            return null;
+        }
+        return currencyRateDTO;
+    }
+
+    public CurrencyRateDTO updateCurrencyRate(Long id, BigDecimal exchangeRate) {
+        CurrencyRate currencyRate = currencyRateRepository.findById(id).orElse(null);
+        if (currencyRate == null) return null;
+        currencyRate.setExchangeRate(exchangeRate);
+        currencyRate.setDate(new Date());
+        try {
+            currencyRate = currencyRateRepository.save(currencyRate);
+        } catch (RuntimeException e) {
+            return null;
+        }
+        return currencyRateMapper.fromEntityToDTO(currencyRate);
+    }
+    
+    public void deleteCurrencyRate(Long id) {
+        currencyRateRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/CurrencyRates/services/CurrencyService.java
@@ -0,0 +1,75 @@
+package com.example.CurrencyRates.services;
+
+import com.example.CurrencyRates.dto.CurrencyDTO;
+import com.example.CurrencyRates.entities.Currency;
+import com.example.CurrencyRates.repositories.CurrencyRepository;
+import com.example.CurrencyRates.utils.mappers.CurrencyMapper;
+import jakarta.persistence.NonUniqueResultException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CurrencyService {
+
+    @Autowired
+    private CurrencyMapper currencyMapper;
+    @Autowired
+    private CurrencyRepository currencyRepository;
+
+    public List<CurrencyDTO> getCurrencies() {
+        return currencyRepository.findAll()
+                .stream()
+                .map(currencyMapper::fromEntityToDTO)
+                .toList();
+    }
+
+    public CurrencyDTO getCurrency(Long id) {
+        return currencyMapper.fromEntityToDTO(
+                currencyRepository.findById(id).orElse(null)
+        );
+    }
+
+    public CurrencyDTO getCurrency(String code) {
+        return currencyMapper.fromEntityToDTO(
+                currencyRepository.findByCode(code.toUpperCase()).orElse(null)
+        );
+    }
+
+    public CurrencyDTO addCurrency(CurrencyDTO currencyDTO) {
+        currencyDTO.setCode(currencyDTO.getCode().toUpperCase());
+        Currency currency = currencyMapper.fromDTOtoEntity(currencyDTO);
+        try {
+            currencyDTO = currencyMapper.fromEntityToDTO(
+                    currencyRepository.save(currency)
+            );
+        } catch (DataIntegrityViolationException e) {
+            return null;
+        }
+        return currencyDTO;
+    }
+
+    public CurrencyDTO updateCurrency(Long id, CurrencyDTO currencyDTO) {
+        if (!currencyRepository.existsById(id)) return null;
+        currencyDTO.setId(id);
+        currencyDTO.setCode(currencyDTO.getCode().toUpperCase());
+        Currency currency = currencyMapper.fromDTOtoEntity(currencyDTO);
+        try {
+            currencyDTO = currencyMapper.fromEntityToDTO(
+                    currencyRepository.save(currency)
+            );
+        } catch (DataIntegrityViolationException e) {
+            return null;
+        }
+        return currencyDTO;
+    }
+
+    public void deleteCurrency(Long id) {
+        currencyRepository.deleteById(id);
+    }
+    public void deleteCurrency(String code) {
+        currencyRepository.deleteByCode(code.toUpperCase());
+    }
+}

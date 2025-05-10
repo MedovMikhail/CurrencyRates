8faac45eaa9ef9dabe0699ccf56f266f1febad03
medovmisha <medovmikhail03@mail.ru>
2025-05-10 15:31:04 +0300

    add Kafka config, producer, consumer, sender and listener

---
+++ src/main/java/com/example/CurrencyRates/kafka/consumer/KafkaMessageListener.java
@@ -0,0 +1,40 @@
+package com.example.CurrencyRates.kafka.consumer;
+
+import com.example.CurrencyRates.dto.kafka.CurrencyCodesMessage;
+import com.example.CurrencyRates.kafka.producer.KafkaSender;
+import com.example.CurrencyRates.services.CurrencyRateService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KafkaMessageListener {
+
+    @Autowired
+    private CurrencyRateService currencyRateService;
+    @Autowired
+    private KafkaSender kafkaSender;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "request-currency-rate", groupId = "group2")
+    void listenerRequiredCurrencyRate(ConsumerRecord<String, String> record) {
+        log.info("Received message [{}] in group1", record.value());
+
+        try {
+            CurrencyCodesMessage codesMessage = objectMapper.readValue(record.value(), CurrencyCodesMessage.class);
+            kafkaSender.sendMessage(
+                    currencyRateService.getCurrenciesScale(codesMessage).toString(),
+                    "get-currency-rate",
+                    record.key());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/example/CurrencyRates/kafka/producer/KafkaSender.java
@@ -0,0 +1,29 @@
+package com.example.CurrencyRates.kafka.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KafkaSender {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message, String topicName) {
+        logMessage(message);
+        kafkaTemplate.send(topicName, message);
+    }
+
+    public void sendMessage(String message, String topicName, String key) {
+        logMessage(message);
+        kafkaTemplate.send(topicName, key, message);
+    }
+
+    private void logMessage(String message) {
+        log.info("Sending : {}", message);
+        log.info("--------------------------------");
+    }
+}

99f864b2815961434332eee1be3fa34c80097054
medovmisha <medovmikhail03@mail.ru>
2025-05-11 14:53:25 +0300

    add kafka listener method to getting currency codes and send currency rates

--- src/main/java/com/example/CurrencyRates/kafka/consumer/KafkaConsumerListener.java
+++ src/main/java/com/example/CurrencyRates/kafka/consumer/KafkaConsumerListener.java
@@ -11,6 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @Component
 @Slf4j
 public class KafkaConsumerListener {
@@ -32,6 +37,20 @@ public class KafkaConsumerListener {
                     currencyRateService.getCurrenciesScale(codesMessage).toString(),
                     "get-currency-rate",
                     record.key());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @KafkaListener(topics = "currency-rates", groupId = "group1")
+    void listenerCurrencyRates(ConsumerRecord<String, String> record) {
+        log.info("Received message [{}] in group1", record.value());
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<String> currencyCodes = objectMapper.readValue(record.value(), ArrayList.class);
+            HashMap<String, BigDecimal> currencyRates = currencyRateService.getCurrencyRatesByCurrencyCodes(currencyCodes);
+            if (currencyRates == null) return;
+            kafkaSender.sendMessage(objectMapper.writeValueAsString(currencyRates), "get-currency-rates", record.key());
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
